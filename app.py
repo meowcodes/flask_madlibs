c9a1eb9d6eac47c18a7f850d8ebdfea725b54d0b
meowcodes <meowcodes@gmail.com>
2019-02-13 15:15:47 -0800

    set up flask and flask dbtoolbar

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 from flask import Flask, request
 from stories import Story, story
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
